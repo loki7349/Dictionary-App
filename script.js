ae43ceccc04c16cf328aa07e5aeaa39847446774
loki7349 <lokeshargon19l@gmail.com>
2023-11-13 23:43:43 +0530

    Dictionary App Completed

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const input = document.querySelector('input')
 const btn = document.querySelector('button')
-const dictionary  = document.querySelector('dictionary-app')
+const dictionary  = document.querySelector('.dictionary-app')
 
 // https://api.dictionaryapi.dev/api/v2/entries/en/<word>
 
 async function dictionaryFn(word){
     const res = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
     .then(res => res.json())
-    console.log(res)
+    return res[0]
 }
 
 btn.addEventListener('click', fetchandCreateCard)
@@ -16,4 +16,39 @@ async function fetchandCreateCard(){
     const data = dictionaryFn(input.value)
 
     console.log(data);
+
+    let partofSpeechArray = []
+
+    for(let i=0; i<data.meanings.length-1; i++){
+        partofSpeechArray.push(data.meanings[i].partOfSpeech)
+    }
+
+    dictionary.innerHTML = `
+    <div class="card">
+                <div class="property">
+                    <span>Word:</span>
+                    <span>${data.word}</span>
+                </div>
+                <div class="property">
+                    <span>Phenotics:</span>
+                    <span>${data.phonetic}</span>
+                </div>
+                <div class="property">
+                    <span>
+                        <audio controls src="${data.phonetics[0].audio}"></audio>
+                    </span>
+                </div>
+                <div class="property">
+                    <span>Definition:</span>
+                    <span>${data.meanings[0].definitions[0].definition}</span>
+                </div>
+                <div class="property">
+                    <span>Example:</span>
+                    <span>${data.meanings[1].definitions[0].example}</span>
+                </div>
+                <div class="property">
+                    <span>${partofSpeechArray.map(e => e).join(', ')}</span>
+                </div>
+            </div>
+    `
 }
